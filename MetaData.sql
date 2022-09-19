e1b65e8f11107178156fd2254a6ecbf43b46994b
John Diraviyam <johndiraviyaml7@gmail.com>
2022-09-19 17:49:18 +0530

    Meta Data file is for starting the users microservices using authentication

---
+++ MetaData.sql
@@ -0,0 +1,57 @@
+INSERT INTO bkp_roles(id,name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'ROLE_ADMIN', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_roles(id,name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'ROLE_CONSUMER', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_roles(id,name ,created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'ROLE_THEATRE_ADMIN', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_roles(id,name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'ROLE_CITY_ADMIN', 'system',now(),'system',now(),'Y');
+
+
+INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Action', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Adventure', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Animation', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Comedy', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Drama', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Fantasy', 'system',now(),'system',now(),'Y');
+    INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Horror', 'system',now(),'system',now(),'Y');
+    INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Mystery', 'system',now(),'system',now(),'Y');
+    INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Romance', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_genre(id,genre_name, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Thriller', 'system',now(),'system',now(),'Y');
+
+
+INSERT INTO bkp_languages(id,language, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'English', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_languages(id,language, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Tamil', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_languages(id,language, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Kannada', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_languages(id,language, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Telugu', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_languages(id,language, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Marathi', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_languages(id,language, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Hindi', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_languages(id,language, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Bengali', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_languages(id,language, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Malayalam', 'system',now(),'system',now(),'Y');
+INSERT INTO bkp_languages(id,language, created_by, create_date , modified_by , last_modified_date, active_flag
+) VALUES(uuid(),'Punjabi', 'system',now(),'system',now(),'Y');
+
+
+
+
+
+
+
